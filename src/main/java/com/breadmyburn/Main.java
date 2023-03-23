4f7c769207e04f5f45dccef5f1382d84c69ec13f
Mat <49784687+oflifeandboredom@users.noreply.github.com>
2023-03-23 19:52:08 +0800

    feat: add exception handling

--- src/main/java/com/breadmyburn/Main.java
+++ src/main/java/com/breadmyburn/Main.java
@@ -1,12 +1,11 @@
 package com.breadmyburn;
 
 import com.breadmyburn.character.classes.*;
-import com.breadmyburn.character.weapons.*;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Variable declarations
         Scanner scanner = new Scanner(System.in);
         String decision;
         String name;
@@ -16,29 +15,30 @@ public class Main {
         String action;
         String weapon;
 
-        System.out.println("Hello! Do you want to create a character?");
+        System.out.println("Hello! Do you want to create a character? (Yes/No)");
         decision = scanner.nextLine();
 
         if ("yes".equalsIgnoreCase(decision)) {
-            System.out.println("What is the name of your character?");
-            name = scanner.nextLine();
-            System.out.println("What is the age of your character?");
-            age = Integer.parseInt(scanner.nextLine());
-            System.out.println("Is your character's class a Saber, Paladin, Archer, or Hunter?");
-            jobClass = scanner.nextLine();
+            try {
+                System.out.println("What is the name of your character?");
+                name = scanner.nextLine();
+                System.out.println("What is the age of your character?");
+                age = Integer.parseInt(scanner.nextLine());
+                System.out.println("Is your character's class a Saber, Paladin, Archer, or Hunter?");
+                jobClass = scanner.nextLine();
 
-            switch (jobClass.toLowerCase()) {
-                case "saber" -> character = new Saber(name, age);
-                case "paladin" -> character = new Paladin(name, age);
-                case "archer" -> character = new Archer(name, age);
-                case "hunter" -> character = new Hunter(name, age);
-                default -> throw new IllegalArgumentException();
-            }
+                switch (jobClass.toLowerCase()) {
+                    case "saber" -> character = new Saber(name, age);
+                    case "paladin" -> character = new Paladin(name, age);
+                    case "archer" -> character = new Archer(name, age);
+                    case "hunter" -> character = new Hunter(name, age);
+                    default -> throw new IllegalArgumentException();
+                }
 
-            System.out.println("Congratulations! You have created a new character.\n");
+                System.out.println("Congratulations! You have created a new character.\n");
 
-            loop: while (true) {
-                System.out.println("""
+                actionLoop: while (true) {
+                    System.out.println("""
                     What action do you want your character to do?
                     - Introduce
                     - Greet
@@ -46,47 +46,52 @@ public class Main {
                     - Block (only for Paladins)
                     - Info
                     - Goodbye""");
-                action = scanner.nextLine();
+                    action = scanner.nextLine();
 
-                switch (action.toLowerCase()) {
-                    case "introduce" -> character.introduce();
-                    case "greeting" -> character.greeting();
-                    case "attack" -> {
-                        if (character instanceof Saber saber) {
-                            saber.Slash();
-                        } else if (character instanceof Paladin paladin) {
-                            paladin.Slash();
-                        } else if (character instanceof Archer archer) {
-                            archer.Shoot();
-                        } else if (character instanceof Hunter hunter) {
-                            System.out.println("Do you want to use your [SWORD] or [GUN]?");
-                            weapon = scanner.nextLine();
-                            switch (weapon.toLowerCase()) {
-                                case "sword" -> hunter.Slash();
-                                case "gun" -> hunter.Shoot();
+                    switch (action.toLowerCase()) {
+                        case "introduce" -> character.introduce();
+                        case "greeting" -> character.greeting();
+                        case "attack" -> {
+                            if (character instanceof Saber saber) {
+                                saber.Slash();
+                            } else if (character instanceof Paladin paladin) {
+                                paladin.Slash();
+                            } else if (character instanceof Archer archer) {
+                                archer.Shoot();
+                            } else if (character instanceof Hunter hunter) {
+                                System.out.println("Do you want to use your [SWORD] or [GUN]?");
+                                weapon = scanner.nextLine();
+                                switch (weapon.toLowerCase()) {
+                                    case "sword" -> hunter.Slash();
+                                    case "gun" -> hunter.Shoot();
+                                    default -> System.out.println("That's not an option!");
+                                }
                             }
                         }
-                    }
-                    case "block" -> {
-                        if (character instanceof Paladin paladin) {
-                            paladin.Block();
-                        } else {
-                            System.out.println("Your character can't do that!");
+                        case "block" -> {
+                            if (character instanceof Paladin paladin) {
+                                paladin.Block();
+                            } else {
+                                System.out.println("Your character can't do that!");
+                            }
                         }
-                    }
-                    case "info" -> {
-                        System.out.println("""
+                        case "info" -> {
+                            System.out.println("""
                             Name: %s
                             Age: %d
                             Class: %s
                             """.formatted(character.getName(), character.getAge(), character.getClass().getSimpleName()));
+                        }
+                        case "goodbye" -> {
+                            break actionLoop;
+                        }
+                        default -> throw new IllegalArgumentException();
                     }
-                    case "goodbye" -> {
-                        System.out.println("Goodbye.");
-                        break loop;
-                    }
-                    default -> throw new IllegalArgumentException();
                 }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Please restart the program and try again.");
+            } finally {
+                System.out.println("Goodbye.");
             }
         } else {
             System.out.println("Ok.");
