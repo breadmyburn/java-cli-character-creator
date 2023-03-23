398c0a335abbae309e14853bd1b8e27c66203d11
Mat <49784687+oflifeandboredom@users.noreply.github.com>
2023-03-23 19:59:54 +0800

    chore: add comments regarding the process of main class

--- src/main/java/com/breadmyburn/Main.java
+++ src/main/java/com/breadmyburn/Main.java
@@ -15,9 +15,11 @@ public class Main {
         String action;
         String weapon;
 
+        // Decide if you want to create a character or not
         System.out.println("Hello! Do you want to create a character? (Yes/No)");
         decision = scanner.nextLine();
 
+        // "Yes": Go through with the character creation
         if ("yes".equalsIgnoreCase(decision)) {
             try {
                 System.out.println("What is the name of your character?");
@@ -27,6 +29,7 @@ public class Main {
                 System.out.println("Is your character's class a Saber, Paladin, Archer, or Hunter?");
                 jobClass = scanner.nextLine();
 
+                // Character job class selection
                 switch (jobClass.toLowerCase()) {
                     case "saber" -> character = new Saber(name, age);
                     case "paladin" -> character = new Paladin(name, age);
@@ -48,17 +51,28 @@ public class Main {
                     - Goodbye""");
                     action = scanner.nextLine();
 
+                    // Character Action Selection
                     switch (action.toLowerCase()) {
+                        // Character introduces themselves
                         case "introduce" -> character.introduce();
+                        // Character gives a greeting (depends on character job class)
                         case "greeting" -> character.greeting();
+                        // Character attacks!
                         case "attack" -> {
+                            // Saber class attack
                             if (character instanceof Saber saber) {
                                 saber.Slash();
-                            } else if (character instanceof Paladin paladin) {
+                            }
+                            // Paladin class attack
+                            else if (character instanceof Paladin paladin) {
                                 paladin.Slash();
-                            } else if (character instanceof Archer archer) {
+                            }
+                            // Archer class attack
+                            else if (character instanceof Archer archer) {
                                 archer.Shoot();
-                            } else if (character instanceof Hunter hunter) {
+                            }
+                            // Hunter class attack
+                            else if (character instanceof Hunter hunter) {
                                 System.out.println("Do you want to use your [SWORD] or [GUN]?");
                                 weapon = scanner.nextLine();
                                 switch (weapon.toLowerCase()) {
@@ -68,6 +82,7 @@ public class Main {
                                 }
                             }
                         }
+                        // Character does a "block" action (Paladin only).
                         case "block" -> {
                             if (character instanceof Paladin paladin) {
                                 paladin.Block();
@@ -75,6 +90,7 @@ public class Main {
                                 System.out.println("Your character can't do that!");
                             }
                         }
+                        // Character information is shown
                         case "info" -> {
                             System.out.println("""
                             Name: %s
@@ -82,6 +98,7 @@ public class Main {
                             Class: %s
                             """.formatted(character.getName(), character.getAge(), character.getClass().getSimpleName()));
                         }
+                        // Exit program
                         case "goodbye" -> {
                             break actionLoop;
                         }
