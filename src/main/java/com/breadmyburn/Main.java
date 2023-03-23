d602323145f69f8a24dfeecd425bce39bc6706d0
Mat <49784687+oflifeandboredom@users.noreply.github.com>
2023-03-23 18:15:35 +0800

    feat: add initial character creation

--- src/main/java/com/breadmyburn/Main.java
+++ src/main/java/com/breadmyburn/Main.java
@@ -1,7 +1,41 @@
 package com.breadmyburn;
 
+import com.breadmyburn.character.classes.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String decision;
+        String name;
+        int age;
+        String jobClass;
+        Jobless character = null;
+        String action;
+
+        System.out.println("Hello! Do you want to create a character?");
+        decision = scanner.nextLine();
+
+        if ("yes".equalsIgnoreCase(decision)) {
+            System.out.println("What is the name of your character?");
+            name = scanner.nextLine();
+            System.out.println("What is the age of your character?");
+            age = Integer.parseInt(scanner.nextLine());
+            System.out.println("Is your character's class a Saber, Paladin, Archer, or Hunter?");
+            jobClass = scanner.nextLine();
+
+            switch (jobClass.toLowerCase()) {
+                case "saber" -> character = new Saber(name, age);
+                case "paladin" -> character = new Paladin(name, age);
+                case "archer" -> character = new Archer(name, age);
+                case "hunter" -> character = new Hunter(name, age);
+                default -> throw new IllegalArgumentException();
+            }
+
+            character.greeting();
+        } else {
+            System.out.println("Ok.");
+        }
     }
 }
